18e2cfa3a3a42e07f46e69f7af69394a1e464285
airtoxin <airtoxin@icloud.com>
2019-01-13 18:09:17 +0900

    It works

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-#[macro_use] extern crate vst2;
+#[macro_use]
+extern crate vst2;
 
 use vst2::buffer::AudioBuffer;
-use vst2::plugin::{Plugin, Info};
+use vst2::plugin::{Plugin, Info, Category};
 
-#[derive(Default)]
-struct DigiDist;
+struct DigiDist {
+    threshold: f32
+}
+
+impl Default for DigiDist {
+    fn default() -> DigiDist {
+        DigiDist {
+            threshold: 1.0,
+        }
+    }
+}
 
 impl Plugin for DigiDist {
     fn get_info(&self) -> Info {
@@ -13,8 +23,65 @@ impl Plugin for DigiDist {
             vendor: "airtoxin".to_string(),
             unique_id: 20190113,
 
-            // fill in the rest with the default values
+            inputs: 2,
+            outputs: 2,
+            parameters: 1,
+
+            category: Category::Effect,
+
             ..Info::default()
+        }
+    }
+
+    fn get_parameter_label(&self, index: i32) -> String {
+        match index {
+            0 => "%".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_text(&self, index: i32) -> String {
+        match index {
+            // Convert to a percentage
+            0 => format!("{}", self.threshold * 100.0),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter_name(&self, index: i32) -> String {
+        match index {
+            0 => "Threshold".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_parameter(&self, index: i32) -> f32 {
+        match index {
+            0 => self.threshold,
+            _ => 0.0,
+        }
+    }
+
+    fn set_parameter(&mut self, index: i32, value: f32) {
+        match index {
+            // We don't want to divide by zero, so we'll clamp the value
+            0 => self.threshold = value.max(0.01),
+            _ => (),
+        }
+    }
+
+    fn process(&mut self, buffer: AudioBuffer<f32>) {
+        // For each buffer, transform the samples
+        for (input_buffer, output_buffer) in buffer.zip() {
+            for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
+
+                if *input_sample >= 0.0 {
+                    *output_sample = input_sample.min(self.threshold) / self.threshold;
+                }
+                else {
+                    *output_sample = input_sample.max(-self.threshold) / self.threshold;
+                }
+            }
         }
     }
 }
